0dc2012169488ada00b3826d0368af35c6d45b31
branch10480 <toshiharu.imaeda@gmail.com>
2022-07-09 01:05:10 +0900

    Tutorial Complete

--- src/main/java/com/example/webfluxtutorial/WebFluxTutorialApplication.java
+++ src/main/java/com/example/webfluxtutorial/WebFluxTutorialApplication.java
@@ -2,12 +2,16 @@ package com.example.webfluxtutorial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class WebFluxTutorialApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WebFluxTutorialApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(WebFluxTutorialApplication.class, args);
+        GreetingClient greetingClient = context.getBean(GreetingClient.class);
+
+        System.out.println(">> message = " + greetingClient.getMessage().block());
     }
 
 }
