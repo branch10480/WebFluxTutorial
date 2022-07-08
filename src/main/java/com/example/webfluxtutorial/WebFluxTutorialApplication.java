37e452daa89aadc15b3dc95a3e23276aade0aaf3
branch10480 <toshiharu.imaeda@gmail.com>
2022-07-08 23:16:37 +0900

    First commit

---
+++ src/main/java/com/example/webfluxtutorial/WebFluxTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.webfluxtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFluxTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebFluxTutorialApplication.class, args);
+    }
+
+}
